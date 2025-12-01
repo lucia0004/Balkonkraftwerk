09e2f6d11bc9491303f39d2f903c6fd01558beb6
lucia0004 <146163783+lucia0004@users.noreply.github.com>
2025-12-01 12:33:03 +0100

    Update gui.py

--- gui.py
+++ gui.py
@@ -70,9 +70,8 @@ with col7:
 with col8:
     cost_PV = st.number_input("Preis der gesamten Anlage in €", value=1000)
 
-
-
-
+st.markdown("<div style='margin-top: 100px;'></div>", unsafe_allow_html=True)
+        
 if st.button("▶ Run Simulation"):
     with st.spinner("Simulation running"):
         get_pvgis_hourly(
@@ -320,5 +319,6 @@ if "kpi" in st.session_state:
     #     st.metric("Imported Energy", f"{Import:.1f}")
 
     # st.metric("Einsparung pro Jahr", f"{saving:.1f}")
+
 
 
